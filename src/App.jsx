feb4f1ce8f829ce2d8750339bf65f0c6868d3630
habibulla552272 <mdhabibulla146425@gmail.com>
2025-01-27 17:53:50 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="">Hello world!</h1>
+      <h2 className=" text-3xl text-center ">hey bro how are you</h2>
+    </>
+  );
+}
+
+export default App;
